7b52a43a2f8cd9a79613441203c54e0f97589559
Nikita Arshinov <nikitaarshinov01k@gmail.com>
2025-05-01 19:31:41 +0300

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Никита Аршинов on 1.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
